05a9f2c882091414379c0531b32359d43cb581a8
Tomy Bolanos <tomy.bolanos@gmail.com>
2020-09-30 09:26:53 -0400

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            try
+            {
+                string url = ConfigurationManager.AppSettings["endpoint"];
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    string  content = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(content);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Request error. Details:  " + e.Message);
+            }
+        }
+    }
+}
---
+++ HelloWorld.API.UnitTest/HelloWorldControllerUnitTest.cs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+using Xunit;
+
+
+namespace HelloWorld.API.UnitTest
+{
+    public class HelloWorldControllerUnitTest
+    {
+        [Fact]
+        public void TestHelloWorld()
+        {
+            var controller = new WebAPI.Controllers.HelloWorldController();
+            var response = controller.Get();
+
+            Assert.True(response == "Hello World");
+        }
+    }
+}
